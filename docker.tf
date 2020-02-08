1b88f0840bdb86ca189477aee17b00b291a8194a
ceefit <ceefit@protonmail.com>
2020-02-08 14:41:43 -0500

    Load balance between docker and EC2 and stop tracking tfstate

--- docker.tf
+++ docker.tf
@@ -9,49 +9,11 @@ resource "docker_container" "seedaware-web" {
   rm    = true
   ports {
     internal = 80
+    external = 9999
   }
-  networks_advanced {
-    name = docker_network.ngrok_network.name
-  }
-}
-
-resource "docker_container" "ngrok" {
-  image = docker_image.ngrok.latest
-  name  = "ngrok"
-  start = true
-  rm    = true
-  ports {
-    internal = 4040
-    external = 4040
-  }
-  ports {
-    internal = 80
-    external = 80
-  }
-  networks_advanced {
-    name = docker_network.ngrok_network.name
-  }
-
-  links   = [docker_container.seedaware-web.name]
-  command = ["ngrok", "http", docker_container.seedaware-web.name]
-}
-
-data "external" "ngrok_uri" {
-  program    = ["bash", "get_ngrok_uri.sh"]
-  depends_on = [docker_container.ngrok]
-}
-
-output "ngrok_uri" {
-  value = data.external.ngrok_uri.result
 }
 
 resource "docker_image" "seedaware-web" {
   name         = "seedaware-web:latest"
   keep_locally = false
 }
-
-resource "docker_image" "ngrok" {
-  name         = "wernight/ngrok"
-  keep_locally = false
-}
-
--- fastly.tf
+++ fastly.tf
@@ -7,15 +7,39 @@ resource "fastly_service_v1" "fastly_seedaware_web" {
   }
 
   backend {
-    address = aws_instance.webserver.public_ip
-    name    = "ec2-instance"
-    port    = 80
+    address     = aws_instance.webserver.public_ip
+    name        = "ec2-instance"
+    port        = 80
+    healthcheck = "ec2-healthcheck"
+    max_conn    = 1
   }
 
   backend {
-    address = aws_instance.webserver.public_ip
-    name    = "ec2-instance"
-    port    = 80
+    address       = "terraform.seedaware.com"
+    name          = "docker"
+    port          = 80
+    healthcheck   = "docker-healthcheck"
+    override_host = "terraform.seedaware.com"
+    max_conn      = 1
+  }
+
+  healthcheck {
+    name   = "ec2-healthcheck"
+    host   = aws_instance.webserver.public_ip
+    path   = "/"
+    method = "GET"
+  }
+
+  healthcheck {
+    name   = "docker-healthcheck"
+    host   = "terraform.seedaware.com"
+    path   = "/"
+    method = "GET"
+  }
+
+  cache_setting {
+    name   = "no-cache"
+    action = "pass"
   }
 
   force_destroy = true
